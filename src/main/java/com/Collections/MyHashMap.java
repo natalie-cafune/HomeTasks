b082d332060a9dc64141b889641f902e72d5347f
natalie-cafune <nataly.pavelko@gmail.com>
2019-02-10 13:04:11 +0200

    Add MyHashMap

--- src/main/java/com/Collections/MyHashMap.java
+++ src/main/java/com/Collections/MyHashMap.java
@@ -10,20 +10,75 @@ public class MyHashMap <K,V>{
     MyNode array [];
     private int sizeArray = 0;
 
-    public void put (K key, V value){
-
-    }
-
     public MyHashMap() {
         this.array = new MyNode[SIZE];
     }
 
+    public K put (K key, V value){
+        newMyNode = new MyNode(key, value);
+        int index = newMyNode.hashCode % (array.length -1);
+        if (array[index] == null) {
+            array[index] = newMyNode;
+            sizeArray++;
+        }
+            else {
+                last = array[index];
+                K oldValue = (K) last.value;
+
+                if (newMyNode.hashCode == last.hashCode && (newMyNode.key == last.key || newMyNode.key.equals(last.key)) &&
+                        last.nextNode == null) {
+                    last.value = newMyNode.value;
+                    return oldValue;
+                } else {
+                    while (last.nextNode != null) {
+                        last = last.nextNode;
+                        if (newMyNode.hashCode == last.hashCode && (newMyNode.key == last.key || newMyNode.key.equals(last.key))) {
+                            last.value = newMyNode.value;
+                            return oldValue;
+                        }
+                    }
+                    if (last.nextNode ==null) {
+                        last.nextNode = newMyNode;
+                    }
+                }
+            }
+
+        return (K)key;
+    }
+
+    // returns item by key
+    public Object get (K key){
+        for (int i = 0; i <= sizeArray; i++) {
+            if (array[i] != null) {
+                if (array[i].key == key && array[i].nextNode == null) {
+                    return array[i].value;
+                }
+            }
+        }
+        return null;
+    }
+
+    // removes item by key
+    public void remove (K key){
+        for (int i = 0; i <= array.length -1; i++) {
+            if (array[i] != null) {
+                if (array[i].key == key && array[i].nextNode == null) {
+                    array[i] = null;
+                    sizeArray--;
+                }
+            }
+        }
+    }
+
+
+    // clears the collection
     public void clear(){
         sizeArray = 0;
         array = null;
         last = null;
 
     }
+    // returns the size of the collection
     public int size(){
         return sizeArray;
     }
