5ed96f1ef9790435879a5a57f391e9e60f4ce51a
natalie-cafune <nataly.pavelko@gmail.com>
2019-02-11 18:28:46 +0200

    Main

---
+++ src/main/java/com/Threads/Main.java
@@ -0,0 +1,27 @@
+package com.Threads;
+
+
+import java.util.Scanner;
+import java.util.concurrent.Semaphore;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите кол-во людей: ");
+        int peopleCount = scanner.nextInt();
+        System.out.print("Введите максимальное кол-во : ");
+        int maxAmount = scanner.nextInt();
+
+        Semaphore sem = new Semaphore(maxAmount);
+        for (int i = 0; i < peopleCount; i++) {
+            new Person(sem, i, maxAmount, peopleCount).start();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
