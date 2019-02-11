9871ca6be4fd565feaf69761d52d317cedc0b6de
natalie-cafune <nataly.pavelko@gmail.com>
2019-02-11 23:08:38 +0200

    Add class Fruit

---
+++ src/main/java/com/StoreFruit/Fruit.java
@@ -0,0 +1,59 @@
+package com.StoreFruit;
+
+public class Fruit {
+
+    private FruitType fruitType;
+    private long expirationDate; //срок годности
+    private long dateOfDelivery; //дата поставки
+    private int price;
+
+
+    public Fruit(FruitType fruitType, long expirationDate, long dateOfDelivery, int price) {
+        this.fruitType = fruitType;
+        this.expirationDate = expirationDate;
+        this.dateOfDelivery = dateOfDelivery;
+        this.price = price;
+    }
+
+    public FruitType getFruitType() {
+        return fruitType;
+    }
+
+    public void setFruitType(FruitType fruitType) {
+        this.fruitType = fruitType;
+    }
+
+    public long getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(long expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public long getDateOfDelivery() {
+        return dateOfDelivery;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    /*public void setDateOfDelivery(String dateOfDelivery) {
+        this.dateOfDelivery = dateOfDelivery;
+    }*/
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s", fruitType, expirationDate, dateOfDelivery, price);
+    }
+}
+
+
+
+
+
