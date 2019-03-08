b840a99699157a32d0c3412cfcbb8f55d4f778cd
natalie-cafune <nataly.pavelko@gmail.com>
2019-03-08 18:36:37 +0200

    refactor

--- src/main/java/com/Multithreading/SecondTask/Main.java
+++ src/main/java/com/Multithreading/SecondTask/Main.java
@@ -7,18 +7,24 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        ExecutorService service = Executors.newSingleThreadExecutor();
         CreateArray createArray = new CreateArray(80000000);
-
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of repetitions : ");
         int number = scanner.nextInt();
+
+        threadsPoolMode(number, createArray);
+        threadMode(number, createArray);
+
+    }
+
+    public static void threadsPoolMode(int n, CreateArray createArray) {
         System.out.println("Mode ThreadsPool : ");
 
+        ExecutorService service = Executors.newSingleThreadExecutor();
         Future futureOne = service.submit((Callable) (new ThreadSinCosPool(createArray)));
 
         long timePool = 0;
-        for (int i = 0; i < number; i++) {
+        for (int i = 0; i < n; i++) {
 
             try {
                 long startTime = System.nanoTime();
@@ -36,10 +42,13 @@ public class Main {
         System.out.println("Time spent : " + timePool / 1000000000 + " sec ");
         service.shutdown();
 
+    }
+
+    public static void threadMode(int n, CreateArray createArray) throws InterruptedException {
         System.out.println("Mode Thread : ");
         long timeThread = 0;
 
-        for (int k = 0; k < number; k++) {
+        for (int k = 0; k < n; k++) {
             long startTime = System.nanoTime();
 
             ThreadSinCos threadSinCos = new ThreadSinCos(createArray);
@@ -54,6 +63,5 @@ public class Main {
             }
         }
         System.out.println("Time spent : " + timeThread / 1000000000 + " sec ");
-
     }
 }
